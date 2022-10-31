9acb87ec9b7733a826435d95e9d029c051ebabd2
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-10-31 22:32:06 +0300

    8.2.2 Chess

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,6 +16,35 @@ public class Main {
         persons.add("Михаил");
         persons.add("Михаил");
 ;
-        Shop.addPerson(persons);
+        //Shop.addPerson(persons);
+
+        example();
+    }
+
+    public static void example() {
+        List<List<String>> biDemArrList = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            biDemArrList.add(i, new ArrayList<>());
+            for (int j = 0; j < 8; j++) {
+                if (i % 2 != 0 && j % 2 != 0) {
+                    biDemArrList.get(i).add(j, "◯");
+                }else if (i % 2 != 0 && j % 2 == 0){
+                    biDemArrList.get(i).add(j, "●");
+                } else if (i % 2 == 0 && j % 2 != 0) {
+                    biDemArrList.get(i).add(j, "●");
+                } else {
+                    biDemArrList.get(i).add(j, "◯");
+                }
+            }
+        }
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 8; j++) {
+                while (j < 8) {
+                    System.out.print(biDemArrList.get(i).get(j) + " ");
+                    j++;
+                }
+                System.out.println();
+            }
+        }
     }
 }
