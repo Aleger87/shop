c1666d38a585c4f485472b02a988fc24fed727fa
Aleksandr Gerb <aleksandrgerb87@gmail.com>
2022-10-31 21:10:47 +0300

    8.2.2 Shop

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        
+        LinkedList<String> persons = new LinkedList<>();
+        persons.add("Люба");
+        persons.add("Семен");
+        persons.add("Степан");
+        persons.add("Федор");
+        persons.add("Лиза");
+        persons.add("Галя");
+        persons.add("Владимир");
+        persons.add("Дмитрий");
+        persons.add("Михаил");
+        persons.add("Михаил");
+;
+        Shop.addPerson(persons);
     }
 }
